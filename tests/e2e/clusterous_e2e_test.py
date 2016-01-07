0e2fd8c6258048b84ecbd4e472a0cfa738d7c935
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2016-01-08 10:16:05 +1100

    BDKD-365 Enhanced tests

--- tests/e2e/clusterous_e2e_test.py
+++ tests/e2e/clusterous_e2e_test.py
@@ -14,67 +14,58 @@ class ClusterE2ETest(unittest.TestCase):
         rc = main(cmd.split())
         return rc, stdout.getvalue(), stderr.getvalue()
 
+    def _cluster_destroy(self):
+        rc, stdout, stderr = self._cli_run('destroy --confirm')
+        return rc == 0 and self._no_cluster_running()
+
+    def _no_cluster_running(self):
+        rc, stdout, stderr = self._cli_run('status')
+        return rc == 0 and 'No working cluster has been set' in stderr
+
+    def _cluster_running(self):
+        rc, stdout, stderr = self._cli_run('status')
+        return rc == 0 and 'e2etestcluster' in stdout and 'running' in stdout
+
+    def _cluster_wrong_state(self):
+        rc, stdout, stderr = self._cli_run('status')
+        return rc == 0 and 'destroy' in stderr
+
     def test_cluster_create(self):
-        # Check cluster status
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if ('No working cluster has been set' in stderr):
-            cmd = 'create data/test-cluster.yml'
-            rc, stdout, stderr = self._cli_run(cmd)
-            self.assertEqual(rc, 0)
-            self.assertRegexpMatches(stderr, 'Cluster "e2etestcluster" created')
+        # Destroy if cluster present
+        if self._cluster_running() or self._cluster_wrong_state():
+            self._cluster_destroy()
+
+        # Create cluster
+        rc, stdout, stderr = self._cli_run('create data/test-cluster.yml')
+        self.assertEqual(rc, 0)
+        self.assertRegexpMatches(stderr, 'Cluster "e2etestcluster" created')
+        self.assertTrue(self._cluster_running())
  
-            # Check if cluster has been created
-            cmd = 'status'
-            rc, stdout, stderr = self._cli_run(cmd)
-            self.assertTrue('e2etestcluster' in stdout and 'running' in stdout)
-
     def test_cluster_status(self):
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if ('No working cluster has been set' in stderr):
-            self.assertRegexpMatches(stderr, 'No working cluster has been set')
-        else:
+        if self._cluster_running():
+            rc, stdout, stderr = self._cli_run('status')
             self.assertTrue('e2etestcluster' in stdout and 'running' in stdout)
 
-    def test_cluster_run(self):
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if ('e2etestcluster' in stdout and 'running' in stdout):
-            cmd = 'run  data/cluster-envs/basic-python-env.yml'
-            rc, stdout, stderr = self._cli_run(cmd)
-            self.assertRegexpMatches(stderr, '1 nodes of type "worker" added')
+#     def test_cluster_run(self):
+#         if self._cluster_running():
+#             rc, stdout, stderr = self._cli_run('run  data/cluster-envs/basic-python-env.yml')
+#             self.assertRegexpMatches(stderr, '1 nodes of type "worker" added')
 
     def test_cluster_add_nodes(self):
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if ('e2etestcluster' in stdout and 'running' in stdout):
-            cmd = 'add-nodes 1'
-            rc, stdout, stderr = self._cli_run(cmd)
+        if self._cluster_running():
+            rc, stdout, stderr = self._cli_run('add-nodes 1')
             self.assertRegexpMatches(stderr, '1 nodes of type "worker" added')
   
     def test_cluster_rm_nodes(self):
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if ('e2etestcluster' in stdout and 'running' in stdout):
-            cmd = 'rm-nodes 1'
-            rc, stdout, stderr = self._cli_run(cmd)
+        if self._cluster_running():
+            rc, stdout, stderr = self._cli_run('rm-nodes 1')
             self.assertRegexpMatches(stderr, '1 nodes of type "worker" removed')
 
     def test_cluster_destroy(self):
-        # Check cluster status
-        cmd = 'status'
-        rc, stdout, stderr = self._cli_run(cmd)
-        if (('e2etestcluster' in stdout and 'running' in stdout) or ('destroy' in stderr)):
-            # Destroy cluster
-            cmd = 'destroy --confirm'
-            rc, stdout, stderr = self._cli_run(cmd)
+        if self._cluster_running() or self._cluster_wrong_state():
+            rc, stdout, stderr = self._cli_run('destroy --confirm')
             self.assertEqual(rc, 0)
-  
-            # Check if cluster has been destroyed
-            cmd = 'status'
-            rc, stdout, stderr = self._cli_run(cmd)
-            self.assertRegexpMatches(stderr, 'No working cluster has been set')
+            self.assertTrue(self._no_cluster_running())
 
 if __name__ == '__main__':
     unittest.main()
