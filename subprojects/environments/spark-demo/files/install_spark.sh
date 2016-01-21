7c4d287711ec06b19ad499f13797e2a8b241a225
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2016-01-21 16:23:19 +1100

    BDKD-441 Updated scripts

--- subprojects/environments/spark-demo/files/install_spark.sh
+++ subprojects/environments/spark-demo/files/install_spark.sh
@@ -1,13 +1,12 @@
-# Spark binaries
-wget -nc http://archive.apache.org/dist/spark/spark-1.4.0/spark-1.4.0-bin-hadoop2.4.tgz
-tar zxvf spark-1.4.0-bin-hadoop2.4.tgz
-mv spark-1.4.0-bin-hadoop2.4 /home/data/spark
+#!/bin/sh
 
-# Spark configuration
-mv /home/data/files/spark-env.sh /home/data/spark/conf
-
-# Http server
-mkdir -p /home/data/http
-wget -nc http://www.apache.org/dyn/closer.lua/spark/spark-1.4.0/spark-1.4.0-bin-hadoop2.4.tgz
-mv spark-1.4.0-bin-hadoop2.4.tgz /home/data/http
-cd /home/data/http; python -m SimpleHTTPServer 8881
+# Spark 
+if [ ! -d /home/data/spark ]; then
+	wget -nc http://archive.apache.org/dist/spark/spark-1.4.0/spark-1.4.0-bin-hadoop2.4.tgz
+	tar zxvf spark-1.4.0-bin-hadoop2.4.tgz
+	mv spark-1.4.0-bin-hadoop2.4 /home/data/spark
+    mv /home/data/files/spark-env.sh /home/data/spark/conf
+	mv spark-1.4.0-bin-hadoop2.4.tgz /home/data/http
+else
+    echo "Spark already configured"
+fi
