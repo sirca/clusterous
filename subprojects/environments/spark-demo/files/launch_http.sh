1f28aa60ddbe2caf47ba70303a8048848fa58e91
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2016-01-22 16:35:27 +1100

    BDKD-441 Spark environment

--- subprojects/environments/spark-demo/files/launch_http.sh
+++
@@ -1,2 +0,0 @@
-mkdir -p /home/data/http
-cd /home/data/http; python -m SimpleHTTPServer 8881
--- subprojects/environments/spark-demo/files/launch_ipython.sh
+++
@@ -1,7 +0,0 @@
-# Spark
-source /home/data/files/install_spark.sh
-
-# Launch ipython notebook
-export SPARK_HOME=/home/data/spark
-export PYTHONPATH=$SPARK_HOME/python:$SPARK_HOME/python/lib/py4j-0.8.2.1-src.zip
-ipython notebook --no-browser --port 31888 --ip=* --notebook-dir=/home/data/files/notebook
