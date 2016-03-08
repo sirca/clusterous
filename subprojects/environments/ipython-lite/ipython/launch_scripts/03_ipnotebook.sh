12b1aed7f9fc7cce7002e082ad7370b116b21b7a
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2016-03-09 10:51:07 +1100

    BDKD-474 IPython Paralell Fix

--- subprojects/environments/ipython-lite/ipython/launch_scripts/03_ipnotebook.sh
+++ subprojects/environments/ipython-lite/ipython/launch_scripts/03_ipnotebook.sh
@@ -1,5 +1,4 @@
-export IPYTHON_PROFILE=/home/data/ipython/profile
 export IPYTHON_DATA=/home/data/ipython/notebook
 
 cd $IPYTHON_DATA
-ipython2 notebook --no-browser --port 8888 --ip=* --profile-dir=$IPYTHON_PROFILE
+ipython2 notebook --no-browser --port 8889 --ip=*
