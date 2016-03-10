fe2150bb1aa865eefea203c2b1e9c79385fafd06
Lolo Fernandez <lolo.fernandez@sirca.org.au>
2016-03-10 14:30:46 +1100

    BDKD-391 Making language consistent

--- clusterous/cluster.py
+++ clusterous/cluster.py
@@ -1430,7 +1430,7 @@ class AWSCluster(Cluster):
                                    hosts_file=os.path.expanduser(defaults.current_nat_ip_file))
         vars_file.close()
         self._logger.debug('Finished sync folder')
-        return (True, 'Ok')
+        return (True, '')
 
 
     def sync_get(self, local_path, remote_path):
@@ -1453,7 +1453,7 @@ class AWSCluster(Cluster):
         stdin, stdout, stderr = ssh.exec_command(cmd)
         output_content = stdout.read()
         if 'cannot access' in stderr.read():
-            message = "Error: Folder '{0}' does not exists.".format(remote_path)
+            message = "Error: Folder '{0}' does not exist.".format(remote_path)
             return (False, message)
 
         src_path = remote_path
@@ -1470,7 +1470,7 @@ class AWSCluster(Cluster):
                                    hosts_file=os.path.expanduser(defaults.current_nat_ip_file))
         vars_file.close()
         self._logger.debug('Finished sync folder')
-        return (True, 'Ok')
+        return (True, '')
 
     def ls(self, remote_path):
         """
@@ -1512,7 +1512,7 @@ class AWSCluster(Cluster):
             message = "Error: Failed to delete folder '{0}'.".format(remote_path)
             return (False, message)
 
-        return (True, 'Ok')
+        return (True, '')
 
 
     def workon(self):
@@ -1692,14 +1692,14 @@ class AWSCluster(Cluster):
                 self._logger.debug(message)
                 return (False, message)
 
-        return (True, 'Ok')
+        return (True, '')
 
     def connect_to_central_logging(self):
         """
         Creates an SSH tunnel to the logging system
         """
         if not self.get_central_logging_ip():
-            message = 'No logging system has been set'
+            message = 'No logging system exists'
             return (True, message)
 
         central_logging_port = defaults.central_logging_port
@@ -1707,7 +1707,7 @@ class AWSCluster(Cluster):
         success = self.create_permanent_tunnel_to_controller(central_logging_port, central_logging_port)
 
         if not success:
-            message = 'Failed create tunnel to centralized logging system'
+            message = 'Failed to create tunnel to central logging system'
             self._logger.debug(message)
             return (False, message)
 
